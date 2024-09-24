3030dc683b5b2b2b1a08d95812a277dc627eddf4
Ali Sajedi Shahgoli <Maj.crosby878@gmail.com>
2024-09-24 13:25:08 +0330

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,28 @@
 #include <Arduino.h>
 
-#define LED 2
+#define TOUCH 22
 
-int TIME1;
-bool GOT_THE_DATA;
-bool led_stat = false;
+
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
-  pinMode(LED,OUTPUT);
-   Serial.println("BOOTED");
+    Serial.begin(115200);
+    pinMode(TOUCH,INPUT);
+
+
+
+
+
  
 }
 
 void loop() {
-  if (GOT_THE_DATA == false)
-  {
-    TIME1 = millis();
-    GOT_THE_DATA = true;
-  }
+ 
+ delay(1000);
+ if (digitalRead(TOUCH))
+ {
+    Serial.println("MMD & SAM");
 
-  if (millis() - TIME1 >= 2000)
-  {
-    if(led_stat == false)
-    {
-      digitalWrite(LED,HIGH);
-      led_stat = true;
-    }
-    else {
-      digitalWrite(LED,LOW);
-      led_stat = false;
-    }
-    GOT_THE_DATA = false;
-  }
-  Serial.println("HELLO RESA");
-  delay(1000);
-
+ }
 
 
 }
